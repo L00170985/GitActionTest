48e56f7e442082a1165145dc865b42a96b241b9d
L00170985 <113893740+L00170985@users.noreply.github.com>
2022-10-26 11:27:08 +0100

    Update Numbers.java

--- src/main/java/ie/atu/numbers/Numbers.java
+++ src/main/java/ie/atu/numbers/Numbers.java
@@ -1,63 +1,6 @@
-package ie.atu.numbers;
-
-//import java.util.Scanner;
-//Added a comment line to test
-//Test
-
-// Added changes in Feature1 Branch to test
-
-// Another test for merge conflicts
-
-//to test the pull request from GitHub UI
-
-// Testing merge conflicts
-
-// Testing the merge conflict on collabroator repo1
-
-//Test merge conflicts for Aravind
-
-//Test1
-<<<<<<< HEAD
-//Test2
-//Test3
-
-=======
->>>>>>> parent of c4cb338 (Test2)
-
 public class Numbers {
-
-	//@SuppressWarnings("resource")
-	public int sum(int a , int b ){
-		
-		return a+b; 
-	}
-	
-	public  int subtract(int a , int b){
-		return a-b;
-	
-	}
-	
-	public int largest(int n1, int n2){
-		if(n1>n2) {
-			return n1;
-		}
-		else {
-			return n2;
-		}
-	}
-
-	/*public static int smallest(){
-		return 1;
-		
-	}
-	*/
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Numbers abcd = new Numbers();
-		int xyz = abcd.sum(2,3);
-		System.out.println("Sum="+xyz);
-
-
-	}
-
+ public static void main(String[] args) {
+ // Prints "Hello, LYIT from ARUN JAYARAJAN" as output
+ System.out.println("Hello, LYIT from ARUN JAYARAJAN");
+ }
 }
